d1e24d86c6aa2bb2feb0e6533a3c4973a7f673f0
Brandon Ford <baford@iastate.edu>
2021-02-23 15:44:30 -0600

    Add stopNoise method to MainActivity

--- AlarmClock/app/src/main/java/com/example/alarmclock/MainActivity.java
+++ AlarmClock/app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -9,23 +9,25 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public MainActivity getContext() {
-        return this;
-    }
-
     public void makeNoise(View view)
     {
         try {
-            MediaPlayer mp = MediaPlayer.create(this, R.raw.loud_alarm_clock);
+            mp = MediaPlayer.create(this, R.raw.loud_alarm_clock);
             mp.start();
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void stopNoise(View view) {
+        mp.release();
     }
 }
