e893f54661d7959e4b6e05b205aff3897d0411fc
SimeonSteward <simeontsteward@gmail.com>
2021-03-09 15:09:57 -0600

    Merge remote-tracking branch 'origin/fileSystem' into fileSystem
    # Conflicts:
    #	AlarmClock/app/src/main/AndroidManifest.xml

--- AlarmClock/app/src/main/java/com/example/alarmclock/MainActivity.java
+++ AlarmClock/app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.alarmclock;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button goToAlarmPage;
@@ -33,4 +33,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void fileManager(View view) {
+        Intent in = new Intent(Intent.ACTION_GET_CONTENT);
+        in.setType("*/*");
+        startActivityForResult(in,1);
+    }
+
+    public void sendMessage(View view) {
+    }
 }
