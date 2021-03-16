9eddb0ee9e915ee9d7c5e2ec5d49ce9ed64eb706
Evan-Hanson-2020 <ejhanson@iastate.edu>
2021-03-16 15:59:14 -0500

    Working on display

--- AlarmClock/app/src/main/java/com/example/alarmclock/TimePickerFragment.java
+++ AlarmClock/app/src/main/java/com/example/alarmclock/TimePickerFragment.java
@@ -2,8 +2,10 @@ package com.example.alarmclock;
 
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.TimePicker;
@@ -39,5 +41,14 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
         long time = alarmTime.getTimeInMillis();
         Alarm alarm = new Alarm();
         alarm.setAlarm(this.getContext(), time);
+
     }
+   public void onClick(View v){
+       Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+       intent.putExtra(AlarmClock.EXTRA_HOUR, Integer.parseInt(alarmTime.toString()));
+       intent.putExtra(AlarmClock.EXTRA_MINUTES, Integer.parseInt(alarmTime.toString()));
+
+
+   }
+
 }
