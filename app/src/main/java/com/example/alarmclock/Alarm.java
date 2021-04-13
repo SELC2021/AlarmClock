84901855c1806fd2f44a5488ed3e54529f3e6ab6
Brandon Ford <baford@iastate.edu>
2021-04-13 14:32:52 -0500

    Add functionality for notifications

--- app/src/main/java/com/example/alarmclock/Alarm.java
+++ app/src/main/java/com/example/alarmclock/Alarm.java
@@ -5,9 +5,11 @@ import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
@@ -22,7 +24,8 @@ import androidx.core.app.NotificationManagerCompat;
 import java.util.Random;
 
 public class Alarm extends BroadcastReceiver {
-    private final String CHANNEL_ID = "";
+    private final int notification_id = new Random().nextInt();
+    private NotificationManagerCompat notificationManager;
     private PendingIntent pi;
     private MediaPlayer mp;
     private Uri uriSound;
@@ -33,20 +36,20 @@ public class Alarm extends BroadcastReceiver {
         @SuppressLint("InvalidWakeLockTag") PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "");
         wl.acquire();
 
-        Toast.makeText(context, "Alarm", Toast.LENGTH_LONG).show(); // For example
+        //Toast.makeText(context, "Alarm!!!", Toast.LENGTH_LONG).show(); // For example
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "alarmChannelID")
-                .setContentTitle("Alarm!!!")
-                .setContentText("This is an alarm.")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pi);
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NotificationChannel.DEFAULT_CHANNEL_ID)
+//                .setContentTitle("Alarm!!!")
+//                .setContentText("This is an alarm.")
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                .setContentIntent(pi)
+//                .setAutoCancel(true);
+//
+//        notificationManager.notify(notification_id, builder.build());
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        alarmNotification(context);
 
-        Random notification_id = new Random();
-        notificationManager.notify(notification_id.nextInt(100), builder.build());
-
-        makeNoise(context);
+        //makeNoise(context);
 
         wl.release();
     }
@@ -58,8 +61,7 @@ public class Alarm extends BroadcastReceiver {
         pi = PendingIntent.getBroadcast(context, 0, i, 0);
         am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 1, pi); // 1000 * 60 * minute interval
 
-        Toast toast = Toast.makeText(context, "Alarm", Toast.LENGTH_LONG);
-        toast.show();
+        //createNotificationChannel(context);
     }
 
     public void cancelAlarm(Context context) {
@@ -73,14 +75,15 @@ public class Alarm extends BroadcastReceiver {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "";
-            String description = "";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            CharSequence name = "AlarmNotificationChanel";
+            String description = "Notification channel for alarms";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(NotificationChannel.DEFAULT_CHANNEL_ID, name, importance);
             channel.setDescription(description);
+            channel.enableVibration(true);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager = context.getSystemService(NotificationManagerCompat.class);
             notificationManager.createNotificationChannel(channel);
         }
     }
@@ -106,5 +109,39 @@ public class Alarm extends BroadcastReceiver {
 
     public void stopNoise(View view) {
         mp.release();
+    }
+
+    private void alarmNotification(Context context)
+    {
+        int NOTIFICATION_ID = 234;
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String CHANNEL_ID = "my_channel_01";
+            CharSequence name = "my_channel";
+            String Description = "Notification channel for alarm app.";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NotificationChannel.DEFAULT_CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Alarm!!!")
+                .setContentText("This is an alarm.");
+
+        Intent resultIntent = new Intent(context, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
